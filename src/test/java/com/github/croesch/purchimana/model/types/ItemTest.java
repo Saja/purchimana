1dd35c7f750b388592f8dbcf0ac181eab8cb45e9
croesch <christianroesch@gmx.net>
2012-11-14 20:34:33 +0100

    New test for testing purpose

---
+++ src/test/java/com/github/croesch/purchimana/model/types/ItemTest.java
@@ -0,0 +1,15 @@
+package com.github.croesch.purchimana.model.types;
+
+import org.junit.Test;
+
+/**
+ * Provides testcases for {@link Item}
+ * 
+ * @author croesch
+ * @since Date: Nov 13, 2012
+ */
+public class ItemTest {
+
+  @Test
+  public void test() {}
+}
