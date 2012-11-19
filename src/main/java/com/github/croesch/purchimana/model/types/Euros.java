e45f4f294b414a61b661436330e0ee05c64918e7
Saja <Saja_92@hotmail.de>
2012-11-19 20:17:02 +0100

    .

---
+++ src/main/java/com/github/croesch/purchimana/model/types/Euros.java
@@ -0,0 +1,44 @@
+package com.github.croesch.purchimana.model.types;
+
+/**
+ * TODO Comment here ...
+ * 
+ * @author Saja
+ * @since Date: 14.11.2012
+ */
+
+public class Euros {
+  public static void main(final String args[]) {
+    int Euros;
+    
+    public Euros {
+      Euros=8;
+      
+    }
+  }
+
+  // TODO Auto-generated constructor stub
+
+  public int getFullEuros() {
+    // TODO Auto-generated method stub
+    return 8;
+  }
+
+  public int getEuroCents() {
+    // TODO Auto-generated method stub
+    return 3;
+  }
+
+  public int inCents() {
+    // TODO Auto-generated method stub
+    return 3;
+  }
+
+  public String asString() {
+    // TODO Auto-generated method stub
+
+    return "ffd";
+
+  }
+
+}
