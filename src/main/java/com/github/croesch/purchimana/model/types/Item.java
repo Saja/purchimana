c71316cd3833ec61f8d161b7979161c1a04e481f
croesch <christianroesch@gmx.net>
2012-11-21 20:40:32 +0100

    Implemented stubs

---
+++ src/main/java/com/github/croesch/purchimana/model/types/Item.java
@@ -0,0 +1,11 @@
+ package com.github.croesch.purchimana.model.types;
+        
+/**
+ * TODO Comment here ...
+ *
+ * @author croesch
+ * @since Date: Nov 21, 2012
+ */
+public interface Item {
+
+}
---
+++ src/main/java/com/github/croesch/purchimana/model/types/LiquidItem.java
@@ -0,0 +1,25 @@
+package com.github.croesch.purchimana.model.types;
+
+/**
+ * TODO Comment here ...
+ * 
+ * @author croesch
+ * @since Date: Nov 21, 2012
+ */
+public class LiquidItem implements Item {
+
+  public LiquidItem(final String string, final Euros euros) {
+    // TODO Auto-generated constructor stub
+  }
+
+  public Euros getEuros() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public String getName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
